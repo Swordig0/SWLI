db94229b75ae2e068949fe0d97c93241d44eac90
Swordig0 <127114216+Swordig0@users.noreply.github.com>
2023-10-12 14:59:58 +0530

    Arduino_UNO-ATMega328p 3.0

--- Arduino_UNO-ATMega328p.ino
+++ Arduino_UNO-ATMega328p.ino
@@ -1,69 +1,76 @@
-#define trig 2
-#define echo 4
-#define pump 10
-char btd;
-int levl;
+int trig_pin = 8;
+int echo_pin = 9;
+int pump = 11;
+long echotime;   //in micro seconds
+float distance;  //in cm
+float level;     //in cm
+int percentage;  //%
+char btd;        //bluetooth data
 
 void setup() {
- Serial.begin(9600);
- pinMode(echo,INPUT);//echo pin of ultraSonic
- pinMode(trig,OUTPUT);//trig pin of ultraSonic
- pinMode(pump,OUTPUT);// water pump
+  Serial.begin(9600);
+  pinMode(trig_pin, OUTPUT);
+  pinMode(echo_pin, INPUT);
+  pinMode(pump, OUTPUT);
+  digitalWrite(trig_pin, LOW);  //Start with trigger LOW
 }
 
 void loop() {
-  digitalWrite(trig, LOW);
-  delayMicroseconds(2) ;
-  digitalWrite(trig, HIGH);
-  delayMicroseconds(10) ;
-  digitalWrite(trig, LOW);
 
-  long t = pulseIn(echo, HIGH);
-  long depth = t/29/2 ;
-  long perc = (25-depth)/20*100;
+  //trigger a pulse-echo measurement
+  digitalWrite(trig_pin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig_pin, LOW);
 
-  if(perc>=100){
-    levl = "FULL";
-  }
-  else if(perc>=75 & perc<=100){
-    levl = "HIGH";
-  }
-  else if(perc>=50 & perc<=75){
-    levl = "MID";
-  }
-  else if(perc>=25 & depth<=50){
-    levl = "LOW";
-  }
-  else{
-    levl = "EMPTY";
+  //get the result
+  echotime = pulseIn(echo_pin, HIGH);
+  distance = 0.0001 * ((float)echotime * 340.0) / 2.0;
+  level = 23 - distance;
+  percentage = round((level /14) * 100);
+
+  if (percentage <= 5) {
+    Serial.println("*YEMPTY*");
+  } else if (percentage <= 30) {
+    Serial.println("*YLOW*");
+  } else if (percentage <= 60) {
+    Serial.println("*YMEDIUM*");
+  } else if (percentage <= 95) {
+    Serial.println("*YHIGH*");
+  } else if (percentage <= 100) {
+    Serial.println("*YFULL*");
   }
 
-  Serial.print(depth);
-  Serial.println("cm");
+  //send over Bluetooth
+  Serial.println("*T" + String(echotime) + "*");
+  Serial.println("*D" + String(distance, 1) + "*");
+  Serial.println("*W" + String(level) + "*");
+  Serial.println("*X" + String(percentage) + "*");
 
-  if(perc<=10){
-    digitalWrite(pump, HIGH);
-  }
-  if(perc>=100){
-    digitalWrite(pump, LOW);
-  }
-
-  if(Serial.available()){
+  //recieve over Bluetooth
+  if (Serial.available()) {
     btd = Serial.read();
-    Serial.println(btd);     
+    Serial.println(btd);
   }
 
-  if(btd=='1'){
+  if (btd == '1') {  //manual pump control
     digitalWrite(pump, HIGH);
-  }
-  else if(btd=='2'){
+    Serial.println("*PON*");
+    Serial.println("*LR0G255B0*");
+  } else if (btd == '0') {
     digitalWrite(pump, LOW);
+    Serial.println("*POFF*");
+    Serial.println("*LR255G0B0*");
+  } else if (btd == 'A') {  //automatic pump control
+    if (percentage <= 10) {
+      digitalWrite(pump, HIGH);
+      Serial.println("*PON*");
+      Serial.println("*LR0G255B0*");
+    } else if (percentage >= 100) {
+      digitalWrite(pump, LOW);
+      Serial.println("*POFF*");
+      Serial.println("*LR255G0B0*");
+    }
   }
 
-  if(pump==HIGH){
-    Serial.print("pON");
-  }
-  else{
-    Serial.print("pOFF");
-  }
+  delay(400);
 }
