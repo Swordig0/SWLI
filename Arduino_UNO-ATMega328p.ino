ef61a0e8630787782429ac3522efccda489ed727
Swordig0 <127114216+Swordig0@users.noreply.github.com>
2023-10-12 14:35:49 +0530

    Arduino_UNO-ATMega328p 2.0
    Main control condition added.
    Bluetooth communication established.

--- Arduino_UNO-ATMega328p.ino
+++ Arduino_UNO-ATMega328p.ino
@@ -1,12 +1,14 @@
-#define trig 8
-#define echo 7
+#define trig 2
+#define echo 4
+#define pump 10
+char btd;
+int levl;
+
 void setup() {
  Serial.begin(9600);
- pinMode(7,INPUT);//echo pin of ultraSonic
- pinMode(8,OUTPUT);//trig pin of ultraSonic
- pinMode(10,OUTPUT);// relay
- pinMode(9,OUTPUT);// buzzer pin // put your setup code here, to run once:
-
+ pinMode(echo,INPUT);//echo pin of ultraSonic
+ pinMode(trig,OUTPUT);//trig pin of ultraSonic
+ pinMode(pump,OUTPUT);// water pump
 }
 
 void loop() {
@@ -17,17 +19,51 @@ void loop() {
   digitalWrite(trig, LOW);
 
   long t = pulseIn(echo, HIGH);
-  long cm = t/29/2 ;
+  long depth = t/29/2 ;
+  long perc = (25-depth)/20*100;
 
-  Serial.print(cm);
-  Serial.print(\n);
-  delay(10);  
-
-  if(Serial.available()){
-    char val = Serial.read();
-    Serial.println(val);     
+  if(perc>=100){
+    levl = "FULL";
+  }
+  else if(perc>=75 & perc<=100){
+    levl = "HIGH";
+  }
+  else if(perc>=50 & perc<=75){
+    levl = "MID";
+  }
+  else if(perc>=25 & depth<=50){
+    levl = "LOW";
+  }
+  else{
+    levl = "EMPTY";
   }
 
-  if(val=='1')
-    digitalWrite()
+  Serial.print(depth);
+  Serial.println("cm");
+
+  if(perc<=10){
+    digitalWrite(pump, HIGH);
+  }
+  if(perc>=100){
+    digitalWrite(pump, LOW);
+  }
+
+  if(Serial.available()){
+    btd = Serial.read();
+    Serial.println(btd);     
+  }
+
+  if(btd=='1'){
+    digitalWrite(pump, HIGH);
+  }
+  else if(btd=='2'){
+    digitalWrite(pump, LOW);
+  }
+
+  if(pump==HIGH){
+    Serial.print("pON");
+  }
+  else{
+    Serial.print("pOFF");
+  }
 }
